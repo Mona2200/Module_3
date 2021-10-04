ac4dcbe22b2221b2ebf144929a817c790441ffea
Алёна Мелехова <alenamelehova13@gmail.com>
2021-10-04 08:04:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass
+{
+   public static void Main(string[] args)
+   {
+
+	  string myName;
+	  myName = "Tom";
+
+	  Console.WriteLine(myName);
+
+	  Console.ReadKey();
+   }
+}
