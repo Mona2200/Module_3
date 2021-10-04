a39e1c5778fef72587895ed2f92f65d158bc5228
Алёна Мелехова <alenamelehova13@gmail.com>
2021-10-04 12:10:54 +0300

    Программа преобразована в анкету.

--- Program.cs
+++ Program.cs
@@ -4,12 +4,10 @@ class MainClass
 {
    public static void Main(string[] args)
    {
-
-	  string myName;
-	  myName = "Bob";
-
-	  Console.WriteLine(myName);
-
-	  Console.ReadKey();
+	  string myName = "Alyona";
+	  byte age = 18;
+	  bool pet = true;
+	  double shoeSize = 46.5;
+	  Console.WriteLine("My name is {0}\nMy age is {1}\nDo I have a pet? {2}\nMy shoe size is {3}", myName, age, pet, shoeSize);
    }
 }
